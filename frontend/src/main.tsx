43c23e0848f5f588b77965f6b70be9ec5a017e66
Paul Pratt <paulaug804@gmail.com>
2024-10-11 11:52:16 -0500

    brought in react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,21 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./components/App";
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/my-recipe-app/",
+    element: <App/>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
