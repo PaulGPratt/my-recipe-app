9e2490f41bf77f67024ef7db3af634fba5e7bde3
Paul Pratt <paulaug804@gmail.com>
2024-10-16 14:57:04 -0500

    adding more recipes

---
+++ api/migrations/5_yet_more_recipes.up.sql
@@ -0,0 +1,173 @@
+INSERT INTO recipe (
+    id, 
+    title, 
+    ingredients, 
+    instructions, 
+    cook_temp_deg_f, 
+    cook_time_minutes
+) VALUES (
+    '1912b65d-6d77-4f3c-89ad-e4bd6f2fde60',
+    'Taqueria-Style Chicken Taco',
+    '* 1 & 1/2 pounds boneless skinless chicken thighs
+* 1/4 cup homemade taco seasoning or store-bought taco seasoning packet
+* 2 tablespoons olive oil for the pan, divided
+* 2 tablespoons butter, divided
+* 3 tablespoons lime juice
+
+  **For the garnishes:**
+* 12 corn tortillas plus more
+* 1 batch Pico de Gallo
+* 1 bunch cilantro chopped
+* &#x20;1 small white onion chopped
+* &#x20;Mexican Crema or sour cream
+* 1 cup Queso Fresco crumbled
+* 2 limes cut into wedges
+* jalapeno sliced
+* radishes sliced thin
+* restaurant-style salsa
+* avocado tomatillo salsa
+',
+    '1. Place chicken thighs on a plate or in a casserole dish. Sprinkle 1/4 cup Homemade Taco Seasoning over the top and rub it all over the chicken with your hands. Let the chicken marinate for at least 15 minutes (or up to 24 hours in the fridge).
+2. Heat a 12-inch sauté pan over medium-high heat for at least 3 minutes. When it is hot, add 1 tablespoon oil. Swirl to coat. Place each chicken thigh in the pan and cook on medium heat, turning occasionally until the chicken reaches 160°F.
+3. Transfer cooked chicken to a cutting board, tent with foil, and rest for a few minutes. Shred the chicken and transfer to a serving bowl, squeeze the juice from one lime over the top.
+4. Char the tortillas by setting a gas burner on high heat and holding each tortilla over the flame for 5-10 seconds on each side.
+5. Assemble tacos: Layer 1 or 2 tortillas with chicken, garnishes, and sauces. Serve immediately.
+
+
+
+   **Slow Cooker Chicken Tacos:** Follow similar steps, but cook on LOW for 2 and 1/2 hours, then shred.
+',
+    0,
+    150
+);
+
+INSERT INTO recipe (
+    id, 
+    title, 
+    ingredients, 
+    instructions, 
+    cook_temp_deg_f, 
+    cook_time_minutes
+) VALUES (
+    '0c92908e-bd7f-41d8-94d8-6042c53ddf33',
+    'Perfect Blueberry Muffins',
+    '* 5 tablespoons (70 grams) unsalted butter, cold
+* 1/2 cup (100 grams) sugar
+* Finely grated zest from half a lemon
+* 3/4 cup (180 grams) plain unsweetened yogurt or sour cream
+* 1 large egg
+* 1 1/2 teaspoons (7 grams) baking powder
+* 1/4 teaspoon baking soda
+* 1/4 teaspoon fine sea or table salt
+* &#x20;1 1/2 cups (195 grams) all-purpose flour
+* 1 1/4 to 1 1/2 cups (215 to 255 grams) blueberries, fresh or frozen
+* 3 tablespoons (35 grams) turbinado sugar (sugar in the raw)
+',
+    '1. Preheat oven to 375°F. Line a muffin tin with 9 paper liners or spray with nonstick spray.
+2. Melt butter and whisk in sugar, zest, yogurt, and egg until smooth. Stir in baking powder, baking soda, and salt.
+3. Gently fold in flour and blueberries. The batter will be thick.
+4. Divide between 9 muffin cups, sprinkle with turbinado sugar.
+5. &#x20;Bake for 25 to 30 minutes or until tops are golden and a tester inserted into the center comes out clean (except for blueberry goo).
+6. Let cool in pan for 10 minutes, then transfer to a rack to cool completely.
+',
+    375,
+    30
+);
+
+INSERT INTO recipe (
+    id, 
+    title, 
+    ingredients, 
+    instructions, 
+    cook_temp_deg_f, 
+    cook_time_minutes
+) VALUES (
+    '57941cd1-b7a3-46db-b0c1-dc850519c040',
+    'Chewy Oatmeal M&M Cookies',
+    '* 1 cup (16 Tbsp; 226g) unsalted butter, softened to room temperature
+* 1 cup (200g) packed light or dark brown sugar
+* 1/4 cup (50g) granulated sugar
+* 2 large eggs, at room temperature
+* 1 tablespoon pure vanilla extract
+* 1 tablespoon molasses
+* 1 and 1/2 cups (188g) all-purpose flour\n- 1 teaspoon baking soda
+* 1 and 1/2 teaspoons ground cinnamon
+* 1/2 teaspoon salt
+* 3 cups (255g) old-fashioned whole rolled oats
+* 1 and 1/2 cups (240g) M\&Ms
+',
+    '1. Using a hand mixer or stand mixer, cream together softened butter and both sugars on medium speed until smooth. Add eggs and mix on high until combined. Add vanilla and molasses, and mix on high until combined. Set aside.
+2. In a separate bowl, whisk together flour, baking soda, cinnamon, and salt. Slowly mix into wet ingredients, then beat in the oats and M\&Ms.
+3. &#x20;Chill dough for 30-60 minutes in the refrigerator (or up to 2 days).
+4. &#x20;Preheat oven to 350°F (177°C). Roll dough into balls and place on a baking sheet. Bake for 11-12 minutes, or until lightly browned on the sides.
+5. Let cool on the baking sheet for 5 minutes before transferring to a wire rack.
+',
+    350,
+    12
+);
+
+
+INSERT INTO recipe (
+    id, 
+    title, 
+    ingredients, 
+    instructions, 
+    cook_temp_deg_f, 
+    cook_time_minutes
+) VALUES (
+    '9f32a30b-a685-4851-a752-73fa85306dca',
+    'Homemade Taco Seasoning',
+    '* 6 tablespoons cornmeal
+* 2 tablespoons chili powder
+* 1 tablespoon ancho chili powder
+* 1 tablespoon chipotle chili powder
+* 2 tablespoons cumin
+* 2 tablespoons oregano
+* 2 tablespoons dried minced onion
+* &#x20;4 teaspoons garlic powder
+* 4 teaspoons sea salt or kosher salt
+* 2 teaspoons paprika
+* 2 teaspoons smoked paprika
+* 1 teaspoon cocoa powder
+',
+    '1. In a medium bowl, whisk together 6 tablespoons cornmeal, 2 tablespoons chili powder, 1 tablespoon ancho chili powder, 1 tablespoon chipotle chili powder, 2 tablespoons cumin, 2 tablespoons oregano, 2 tablespoons dried minced onion, 4 teaspoons garlic powder, 4 teaspoons sea salt, 2 teaspoons paprika, 2 teaspoons smoked paprika, and 1 teaspoon cocoa powder.
+2. Store the mixture in an airtight container, such as a mason jar or Tupperware.
+3. Use 2 tablespoons of seasoning for 1/2 pound of ground beef, and 1/4 cup for 1 pound of ground beef.
+4. &#x20;To make taco meat: cook 1 pound of ground beef in a pan over medium heat, then add 1/4 cup taco seasoning and 1 1/4 cups of water. Optionally, add 1 teaspoon Better Than Bouillon Beef Base for enhanced flavor. Cook until the liquid is mostly reduced.
+',
+    0,
+    0
+);
+
+INSERT INTO recipe (
+    id, 
+    title, 
+    ingredients, 
+    instructions, 
+    cook_temp_deg_f, 
+    cook_time_minutes
+) VALUES (
+    '21286fd9-a0c1-4159-9fdf-1fce6202d9c6',
+    'Chewy Pumpkin Oatmeal Cookies',
+    '* 1 cup pumpkin puree
+* 2 cups oats (quickly ground in a food processor or blender)
+* 1/4 cup honey (or maple syrup)
+* 2 tablespoons unsalted butter (melted and slightly cooled)
+* 1 egg
+* 1 teaspoon vanilla extract
+* 1 teaspoon baking powder
+* 1/2 teaspoon cinnamon
+* 1/2 teaspoon pumpkin pie spice
+* &#x20;1/2 teaspoon salt
+* 1/2 cup mini chocolate chips (optional)
+',
+    '1. Add the pumpkin puree to a medium bowl. Add the rest of the ingredients, including the chocolate chips (if using). Stir to combine thoroughly with a spoon.
+2. Place batter in the fridge or freezer while the oven preheats to 375°F (190°C).
+3. Line a baking sheet with parchment paper. Scoop and drop 2-tablespoon-size balls of batter onto the prepared baking sheet. Press lightly to flatten to about 1/2-inch thick.
+4. Bake for 14-16 minutes until cookies are lightly golden around the edges and firm to the touch.
+5. Remove from the oven and let cool on the baking sheet for 5 minutes before transferring to a wire rack to cool completely.
+6. Serve immediately or store and serve at room temperature or chilled.
+',
+    375,
+    12
+);
