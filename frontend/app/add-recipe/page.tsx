12078fd13ff987ce01811de7e39fbd337a97a618
Paul Pratt <paulaug804@gmail.com>
2024-12-03 10:21:12 -0600

    move add-recipe-server into the page.tsx

--- frontend/app/add-recipe/page.tsx
+++ frontend/app/add-recipe/page.tsx
@@ -1,10 +1,30 @@
 import { Metadata } from "next";
-import AddRecipeServer from "../../components/add-recipe.server";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import AddRecipeClient from "../../components/add-recipe.client";
+import { getDecodedTokenCookie } from "../../lib/firebase-admin";
 
 export const metadata: Metadata = {
   title: 'Add Recipe',
-}
+};
 
 export default async function AddRecipePage() {
-  return <AddRecipeServer />;
+  const cookieStore = await cookies();
+  const firebaseToken = cookieStore.get("firebaseToken");
+
+  if (!firebaseToken) {
+    redirect("/login");
+  }
+
+  try {
+    const idToken = await getDecodedTokenCookie();
+    if (!idToken) {
+      redirect(`/login`);
+    }
+
+    return <AddRecipeClient />;
+  } catch (error) {
+    console.error("Error validating token:", error);
+    redirect("/login");
+  }
 }
