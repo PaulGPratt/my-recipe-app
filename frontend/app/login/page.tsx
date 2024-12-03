c3909346ed91ae17ad412f952e9696450b264928
Paul Pratt <paulaug804@gmail.com>
2024-12-03 12:35:06 -0600

    continuing to get rid of server components

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,14 +1,23 @@
 import { Metadata } from "next";
-import LoginServer from "../../components/login.server";
+import Link from "next/link";
+import LoginClient from "../../components/login.client";
 
 export const metadata: Metadata = {
   title: 'Login',
-}
+};
 
-function LoginPage() {
+export default function LoginPage() {
   return (
-    <LoginServer />
+    <div className="h-full mx-auto max-w-xl">
+      <form>
+        <div className="p-4 py-6 text-center text-4xl font-semibold">
+          Log in
+        </div>
+        <LoginClient />
+        <div className="p-4 pt-0 text-xl">
+          <Link href="/signup">Sign up for an account</Link>
+        </div>
+      </form>
+    </div>
   );
 }
-
-export default LoginPage;
--- frontend/app/recipes/[username]/[slug]/edit/page.tsx
+++ frontend/app/recipes/[username]/[slug]/edit/page.tsx
@@ -1,15 +1,41 @@
 import { Metadata } from "next";
-import EditRecipeServer from "../../../../../components/edit-recipe.server";
-
-type Params = Promise<{ username: string; slug: string }>
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import EditRecipeClient from "../../../../../components/edit-recipe.client";
+import { getDecodedTokenCookie } from "../../../../../lib/firebase-admin";
+import getRequestClient from "../../../../../lib/get-request-client";
 
 export const metadata: Metadata = {
   title: 'Edit Recipe',
-}
+};
 
-export default async function EditRecipePage(props: {
-  params: Params
-}) {
-  const {username, slug} = await props.params
-  return <EditRecipeServer username={username} slug={slug} />;
+export default async function EditRecipePage({ params }: { params: { username: string; slug: string } }) {
+  const { username, slug } = await params;
+
+  if (!username || !slug) {
+    redirect("/");
+  }
+
+  const cookieStore = await cookies();
+  const firebaseToken = cookieStore.get("firebaseToken");
+
+  if (!firebaseToken) {
+    redirect("/login");
+  }
+
+  const client = getRequestClient(firebaseToken.value);
+
+  try {
+    const recipe = await client.api.GetRecipe(username, slug);
+
+    const idToken = await getDecodedTokenCookie();
+    if (!idToken || !(idToken.uid === recipe.profile_id)) {
+      redirect(`/recipes/${username}/${slug}`);
+    }
+
+    return <EditRecipeClient recipe={recipe} username={username} />;
+  } catch (error) {
+    console.error("Error fetching recipe or validating token:", error);
+    redirect(`/recipes/${username}/${slug}`);
+  }
 }
