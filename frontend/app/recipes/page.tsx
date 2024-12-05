62cf988f82f50471d5d2437c4d2725a9b7acbde2
Paul Pratt <paulaug804@gmail.com>
2024-12-05 13:16:20 -0600

    recipe list metadata

--- frontend/app/recipes/page.tsx
+++ frontend/app/recipes/page.tsx
@@ -1,16 +1,51 @@
 import { Metadata } from "next";
 import RecipeListServer from "../../components/recipe-list.server";
 
-export const metadata: Metadata = {
-  title: 'All Recipes',
-}
+export const generateMetadata = async (): Promise<Metadata> => {
+
+  const baseurl = process.env.NEXT_PUBLIC_BASE_URL;
+  const ogImageUrl = `${baseurl}og-image.jpg`;
+  const xImageUrl = `${baseurl}x-image.jpg`;
+
+  const title = `All Recipes`
+  const description = `Check out these recipes at ${baseurl}!`;
+  return {
+    title: title,
+    description: description,
+    openGraph: {
+      title: title,
+      description: description,
+      images: [
+        {
+          url: ogImageUrl,
+          alt: title,
+          height: 630,
+          width: 1200,
+        },
+      ],
+      url: `${baseurl}/recipes`,
+    },
+    twitter: {
+      title: title,
+      description: description,
+      images: [
+        {
+          url: xImageUrl,
+          alt: title,
+          height: 600,
+          width: 1200,
+        },
+      ],
+    },
+  };
+};
 
 function AllRecipes() {
-    return (
-      <RecipeListServer
-        fetchRecipes={(client) => client.api.GetAllRecipes()}
-      />
-    );
-  }
+  return (
+    <RecipeListServer
+      fetchRecipes={(client) => client.api.GetAllRecipes()}
+    />
+  );
+}
 
-  export default AllRecipes;
+export default AllRecipes;
