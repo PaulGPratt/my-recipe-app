cc8c7c3637d0f93d4706f44efa765a0d4abda827
Paul Pratt <paulaug804@gmail.com>
2024-12-17 15:51:19 -0600

    maybe I do need to force dynamic

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -7,6 +7,8 @@ export const metadata: Metadata = {
     title: 'Home | PrattProjects.com',
 };
 
+export const dynamic = 'force-dynamic';
+
 export default async function HomePage() {
     let topProfiles: api.ProfileRecipe[];
     try {
