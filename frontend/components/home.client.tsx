2cda5c1e9a58e0f04cf6f2ea89768970da029c10
Paul Pratt <paulaug804@gmail.com>
2024-12-17 14:04:30 -0600

    lets rework the navigation now

--- frontend/components/home.client.tsx
+++ frontend/components/home.client.tsx
@@ -10,6 +10,7 @@ import { Button } from "./ui/button";
 import { Plus } from "lucide-react";
 import { api } from "../lib/client";
 import { fetchStoredProfile } from "../lib/profile-utils";
+import { Skeleton } from "./ui/skeleton";
 
 interface HomeClientProps {
     profileRecipes?: api.ProfileRecipe[];
@@ -19,14 +20,17 @@ export default function HomeClient({ profileRecipes }: HomeClientProps) {
     const { auth } = useContext(FirebaseContext);
     const user = auth?.currentUser;
 
+    const [isLoading, setIsLoading] = useState(true);
     const [profile, setProfile] = useState<api.Profile>();
     const [recipesByOthers, setRecipesByOthers] = useState<api.ProfileRecipe[]>();
 
     useEffect(() => {
         const fetchMyProfile = async () => {
+            if (!auth) return;
             if (!auth?.currentUser) {
                 if (profileRecipes) {
                     setRecipesByOthers(profileRecipes);
+                    setIsLoading(false);
                 }
                 return;
             }
@@ -41,7 +45,10 @@ export default function HomeClient({ profileRecipes }: HomeClientProps) {
                 }
             } catch (err) {
                 console.error(err);
+            } finally {
+                setIsLoading(false);
             }
+
         };
         fetchMyProfile();
     }, [auth]);
@@ -68,41 +75,42 @@ export default function HomeClient({ profileRecipes }: HomeClientProps) {
             <Separator></Separator>
             <ScrollArea className="h-full w-full">
                 <div className="p-4 flex">
-                    {profile?.username ? (
-                        <div className="flex flex-row gap-4 w-full">
-                            <Link href={`/recipes/${profile.username}`} passHref className="flex flex-grow">
-                                <Button variant="secondary" className="flex-grow">
-                                    My Recipes
-                                </Button>
-                            </Link>
-                            <Link href={`/add-recipe`} passHref className="flex flex-grow">
-                                <Button variant="secondary" className="flex-grow">
-                                    <Plus></Plus> Add Recipe
-                                </Button>
-                            </Link>
-                        </div>
-                    ) : (
-                        <div className="flex flex-col w-full text-xl">
-                            <div className="pb-2">If you'd like to add any recipes of your own:</div>
-                            <div className="flex flex-row items-center gap-2 w-full">
-                                <Link href={`/login`} passHref className="flex flex-grow">
+                    {isLoading ? (<div className="flex flex-row gap-4 w-full">
+                        <Skeleton className="w-full h-12" />
+                        <Skeleton className="w-full h-12" />
+                    </div>) :
+                        profile?.username ? (
+                            <div className="flex flex-row gap-4 w-full">
+                                <Link href={`/recipes/${profile.username}`} passHref className="flex flex-grow w-full">
                                     <Button variant="secondary" className="flex-grow">
-                                        Log in
+                                        My Recipes
                                     </Button>
                                 </Link>
-                                <div>
-                                    or
-                                </div>
-                                <Link href={`/signup`} passHref className="flex flex-grow">
+                                <Link href={`/add-recipe`} passHref className="flex flex-grow w-full">
                                     <Button variant="secondary" className="flex-grow">
-                                        Sign up
+                                        <Plus></Plus> Add Recipe
                                     </Button>
                                 </Link>
                             </div>
+                        ) : (
+                            <div className="flex flex-col w-full text-xl">
+                                
+                                <div className="flex flex-row items-center gap-4 w-full">
+                                    <Link href={`/login`} passHref className="flex flex-grow w-full">
+                                        <Button variant="secondary" className="flex-grow">
+                                            Log in
+                                        </Button>
+                                    </Link>
+                                    <Link href={`/signup`} passHref className="flex flex-grow w-full">
+                                        <Button variant="secondary" className="flex-grow">
+                                            Sign up
+                                        </Button>
+                                    </Link>
+                                </div>
+                                <div className="pt-2 text-muted-foreground text-center">Log in or Sign up to add your own recipes</div>
+                            </div>
 
-                        </div>
-
-                    )}
+                        )}
                 </div>
 
                 <Separator></Separator>
